2d8bbf977377fc57e2800c451d8fe00606c41997
abojja9 <abojja@uvic.ca>
2019-09-23 20:37:45 -0700

    completed registraion forms

--- application.py
+++ application.py
@@ -3,11 +3,18 @@ import os
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
+from form_fields import *
+
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-socketio = SocketIO(app)
+app.secret_key = 'replace later'
+# app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+# socketio = SocketIO(app)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+
+    reg_form = RegistrationForm()
+    if reg_form.validate_on_submit():
+        return "Great Success!"
+    return render_template('index.html', form=reg_form)

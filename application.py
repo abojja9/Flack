53aa166ce9d78bef1efcae743e96d0b1b3c6a84c
abojja9 <abojja@uvic.ca>
2019-09-24 22:52:26 -0700

    Completed basic functionality

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
 import os
 
 from flask import Flask, render_template, redirect, url_for, flash
-from flask_socketio import SocketIO, emit, send
+from flask_socketio import SocketIO, emit, send, join_room, leave_room
 from passlib.hash import pbkdf2_sha256
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
+from time import localtime, strftime
 
 from form_fields import *
 from models import *
@@ -14,6 +15,9 @@ app.secret_key = 'replace later'
 
 # Initialize flask-socketio
 socketio = SocketIO(app)
+
+# Creating Flack rooms
+ROOMS = ["lunch", "movies", "games", "news"]
 
 # Configure database
 app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://rcrlddecvxotif:fe0adb891142e2d956e6f6209c2472c045f17ffb5e15aef4ee2c20ec525885dd@ec2-174-129-231-116.compute-1.amazonaws.com:5432/da364vd80pj157'
@@ -72,7 +76,9 @@ def chat():
     #     flash('Please Login!', 'danger')
     #     return redirect(url_for('login'))
 
-    return render_template('chat.html')
+    # Remember current user so that we can display him
+    return render_template('chat.html', username=current_user.username,
+    rooms=ROOMS)
 
 @app.route("/logout", methods=["GET"])
 def logout():
@@ -83,16 +89,29 @@ def logout():
 
 @socketio.on('message')
 def message(data):
-    send(data)
+    print (data)
+
+    msg = data["msg"]
+    username = data["username"]
+    room = data["room"]
+    time_stamp = strftime('%b-%d %I:%M%p', localtime())
+    send({"username": username, "msg": msg, "time_stamp": time_stamp}, room=room)
 
 
+@socketio.on('join')
+def join(data):
+
+    join_room(data['room'])
+    send({'msg': data['username'] + ' has joined the ' + data['room'] + " room."}, room=data['room'])
 
 
+@socketio.on('leave')
+def leave(data):
 
+    leave_room(data['room'])
+    send({'msg': data['username'] + ' has left the ' + data['room'] + " room."}, room=data['room'])
 
-
-
-
+ 
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)

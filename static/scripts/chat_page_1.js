60945e8a96ce39ec888d8789e2dbe58da1a48dd7
abojja9 <abojja@uvic.ca>
2019-09-27 18:34:57 -0700

    Basic functionality completed

--- static/scripts/chat_page_1.js
+++ static/scripts/chat_page_1.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    // Make sidebar collapse on click
+    document.querySelector('#show-sidebar-button').onclick = () => {
+        document.querySelector('#sidebar').classList.toggle('view-sidebar');
+    };
+
     // Make 'enter key submit message'
     let msg = document.querySelector('#user_message');
     msg.addEventListener('keyup', event => {
